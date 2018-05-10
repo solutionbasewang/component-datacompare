0642b075ddc54035dc51deb0dd5fb57fabed5c38
solutionbasewang <17781297@qq.com>
2018-05-10 11:35:06 +0800

    update pom.xml update ports(9002-10001)

--- src/main/java/com/goldwind/mxm/goplus/tool/databasecompare/Application.java
+++ src/main/java/com/goldwind/mxm/goplus/tool/databasecompare/Application.java
@@ -2,14 +2,17 @@ package com.goldwind.mxm.goplus.tool.databasecompare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
  * Created by alex on 2017/11/23.
  */
-@EnableTransactionManagement
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
